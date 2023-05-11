eb68002cb6ad7ca80adee2be6a7973cf9bb5d41e
Jahidul Islam <jahidulzaid@gmail.com>
2023-05-11 23:41:27 +0600

    updated 266

--- 266 - A. Stones on the Table.cpp
+++ 266 - A. Stones on the Table.cpp
@@ -9,6 +9,20 @@
         int t;
         cin >> t;
         cin.ignore();
+<<<<<<< HEAD
+
+        while(t--){
+            string j;
+            getline(cin, j);
+            int mined=0;
+            for(int i =0; i<j.size(); i++){
+                if(j[i]==j[i+1]){
+                    mined++;
+                    break;
+                }
+            }
+            cout << mined << endl;
+=======
         string j;
         getline(cin, j);
         int mined=0;
@@ -18,6 +32,7 @@
                 break;
             }
         cout << mined << endl;
+>>>>>>> 79ed3b271756ac1de6c1e5bd873fb4a552f986e1
         }    
         return 0;
     }
