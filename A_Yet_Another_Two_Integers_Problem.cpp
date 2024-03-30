9cbd67c820c424b7ad22f01d8bfe8b4db8ca63a4
Jahidul Islam <jahidulzaid@gmail.com>
2024-03-30 23:44:14 +0600

    solved 1 new problm

--- A_Yet_Another_Two_Integers_Problem.cpp
+++ A_Yet_Another_Two_Integers_Problem.cpp
@@ -1,7 +1,5 @@
 // Bismillahir Rahmanir Rahim
 // jahidulZaid
-// 
-
 #include <bits/stdc++.h>
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -18,11 +16,26 @@ using namespace std;
 // #include "debug.h"
 // #endif
 
+bool dif(ll a, ll b){
+    if(abs(a-b) < 10) return 1;
+    else return 0;
+}
+
 int main() {
     optimize();
-    
-    
+    tt;
+    while(t--){
+        ll a,b; cin >> a >> b;
+        int c = 0;
+        ll diff = abs(a - b);
+        c = diff/10;
+        ll rem = diff%10;
 
-
+        if(rem>0){
+            c++;
+        }
+        cout << c << endl;  
+        
+    }
     return 0;
 }
